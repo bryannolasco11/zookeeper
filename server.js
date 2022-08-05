e535f3e7c899f89619af4064d47cbb5ce81e4d7b
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-05 00:18:35 -0500

    connected to html

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals.json');
 
@@ -116,4 +117,20 @@ app.get('/api/animals', (req, res) => {
   
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
